ebde334ecf05f8525c97622250d865b4f24a4bae
hassanAlihafiz <bali37088@gmail.com>
2022-06-29 01:27:53 +0500

    Add

--- Components/HomeComponents/NoReplyTab/NoReplyCard.js
+++ Components/HomeComponents/NoReplyTab/NoReplyCard.js
@@ -1,136 +1,152 @@
 import React, { useEffect, useState } from 'react';
-import {Dimensions, Image, View, StyleSheet, Text, ScrollView, Touchable, TouchableOpacity } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
 import { NoRepData } from './NoreplyData';
-import {useNavigation} from '@react-navigation/core';
-import rep from '../../../assets/typingIcon.png'
+import { useNavigation } from '@react-navigation/core';
+import rep from '../../../assets/typingIcon.png';
 import { Color } from '../../../Utils/colorfile';
 import firestore from '@react-native-firebase/firestore';
 import { firebase } from '@react-native-firebase/auth';
 import useAuth from '../../../Hooks/useAuth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const WIDTH =Dimensions.get("window").width;
+const WIDTH = Dimensions.get('window').width;
 export default function NoRep() {
-    const [point, setPoint] = useState(null)
-    const [list, setList] = useState([])
-    const [userId, setuserId] = useState()
+  const [point, setPoint] = useState(null);
+  const [list, setList] = useState([]);
+  const [userId, setuserId] = useState();
   const navigation = useNavigation();
-  const {user} = useAuth()
+  const { user } = useAuth();
   const getData = async () => {
     try {
-      const jsonValue = await AsyncStorage.getItem('@userData')
-      const points = await AsyncStorage.getItem('@userPoint')
-      console.log('ff', jsonValue)
-      setuserId(jsonValue)
-      console.log('ffpoi', points)
-      setPoint( parseInt(points))
-      firestore().collection('Users').where( "userId" ,'!=', jsonValue)
-      .onSnapshot(snap =>{
-        //   console.log("users: ", snap.docs.map(do=>do.dat))
-        const data = snap.docs.map(doc => doc.data())
-        setList(data)
-      }) 
-    } catch(e) {
+      const jsonValue = await AsyncStorage.getItem('@userData');
+      const points = await AsyncStorage.getItem('@userPoint');
+      console.log('ff', jsonValue);
+      setuserId(jsonValue);
+      console.log('ffpoi', points);
+      setPoint(parseInt(points));
+      firestore()
+        .collection('Users')
+        .where('userId', '!=', jsonValue)
+        .onSnapshot(snap => {
+          //   console.log("users: ", snap.docs.map(do=>do.dat))
+          const data = snap.docs.map(doc => doc.data());
+          setList(data);
+        });
+    } catch (e) {
       // error reading value
-      console.log(e)
+      console.log(e);
     }
-  }
-  useEffect(()=>{
-    getData()
-      
-    },[])
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <ScrollView>
-      {list.map((data, index)=>(
-      <View style={styles.card} key={index}>
+      {list.map((data, index) => (
+        <View style={styles.card} key={index}>
           <TouchableOpacity style={styles.border}>
-          {/* <Image source={} style={styles.image} /> */}
+            {/* <Image source={} style={styles.image} /> */}
           </TouchableOpacity>
-              <TouchableOpacity
-            //    onPress={()=>navigation.navigate('ChatScreen' ,{recieverId: data.userId},)}
-                style={styles.text}
-               >    
-              <Text style={styles.nameColor}>{data.userId.slice(0, 15)}</Text>
-              {/* <Text style={styles.messColor}>{data.date}</Text> */}
-              {/* <Text style={styles.msgText} >{data.text}</Text> */}
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('ChatScreen', { recieverId: data.userId })
+            }
+            style={styles.text}>
+            <Text style={styles.nameColor}>{data.userId.slice(0, 15)}</Text>
+            {/* <Text style={styles.messColor}>{data.date}</Text> */}
+            {/* <Text style={styles.msgText} >{data.text}</Text> */}
           </TouchableOpacity>
-          {
-             point <=5 ?
-           <TouchableOpacity onPress={()=>console.log('please get point !!', typeof(point))} style={styles.reply}>
-            <Image source={rep} height={21} width={22.83}/>
-          </TouchableOpacity>:             
-          <TouchableOpacity onPress={()=>navigation.navigate('ChatScreen' ,{recieverId: data.userId},)} style={styles.reply}>
-              <Image source={rep} height={21} width={22.83}/>
+          {point <= 5 ? (
+            <TouchableOpacity
+              onPress={() => console.log('please get point !!', typeof point)}
+              style={styles.reply}>
+              <Image source={rep} height={21} width={22.83} />
             </TouchableOpacity>
-          }
-      </View>
-          ))}
-              </ScrollView>
+          ) : (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('ChatScreen', { recieverId: data.userId })
+              }
+              style={styles.reply}>
+              <Image source={rep} height={21} width={22.83} />
+            </TouchableOpacity>
+          )}
+        </View>
+      ))}
+    </ScrollView>
   );
 }
-const styles= StyleSheet.create({
-    card:{
-        marginBottom:12,
-        flex:1,
-        flexDirection:'row',
-        width:WIDTH*0.9,
-        height:WIDTH/4.5,
-        alignSelf:'center',
-        justifyContent:'space-around',
-        // marginRight:2,
-        borderRadius:15,
-        // paddingLeft:10,
-        backgroundColor:Color.SECONDARY_COLOR,
-        shadowColor: Color.BLACK,
-        shadowOffset: {
-            width: 10,
-            height: 5,
-        },
-        elevation: 3,
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 12,
+    flex: 1,
+    flexDirection: 'row',
+    width: WIDTH * 0.9,
+    height: WIDTH / 4.5,
+    alignSelf: 'center',
+    justifyContent: 'space-around',
+    // marginRight:2,
+    borderRadius: 15,
+    // paddingLeft:10,
+    backgroundColor: Color.SECONDARY_COLOR,
+    shadowColor: Color.BLACK,
+    shadowOffset: {
+      width: 10,
+      height: 5,
     },
-    border:{
-        borderColor:Color.PRIMARY_COLOR,
-        borderWidth:2,
-        height:49,
-        width:49,
-        borderRadius:50,
-        marginTop:15,
-    },
-    image:{
-        width:41,
-         height:41,
-         borderRadius:50,
-        margin:2,         
-    }
-    ,
-    text:{
-        marginLeft:-25,
-        marginTop:15,
-        // backgroundColor:Color.PRIMARY_COLOR,
-    },
-    messColor:{
-    color:Color.CARD_TEXT,
-     fontSize:13,
-     fontWeight:'400'
-    },
-    nameColor:{
-    color:Color.CARDNAME,
-    fontWeight:'600', 
-    fontSize:16,
-    width:WIDTH*0.45
-},
-    dateColor:{
-    color:Color.CARD_DATE,
-    fontWeight:'400', 
-    fontSize:13,
-},
-reply:{
-    marginTop:15,
+    elevation: 3,
+  },
+  border: {
+    borderColor: Color.PRIMARY_COLOR,
+    borderWidth: 2,
+    height: 49,
+    width: 49,
+    borderRadius: 50,
+    marginTop: 15,
+  },
+  image: {
+    width: 41,
+    height: 41,
+    borderRadius: 50,
+    margin: 2,
+  },
+  text: {
+    marginLeft: -25,
+    marginTop: 15,
+    // backgroundColor:Color.PRIMARY_COLOR,
+  },
+  messColor: {
+    color: Color.CARD_TEXT,
+    fontSize: 13,
+    fontWeight: '400',
+  },
+  nameColor: {
+    color: Color.CARDNAME,
+    fontWeight: '600',
+    fontSize: 16,
+    width: WIDTH * 0.45,
+  },
+  dateColor: {
+    color: Color.CARD_DATE,
+    fontWeight: '400',
+    fontSize: 13,
+  },
+  reply: {
+    marginTop: 15,
     // marginLeft:35,
-},
-msgText:{
-color:Color.CARD_TEXT,
- fontSize:12
-}
-})
+  },
+  msgText: {
+    color: Color.CARD_TEXT,
+    fontSize: 12,
+  },
+});
--- Screen/ChatScreen/ChatScreen.js
+++ Screen/ChatScreen/ChatScreen.js
@@ -1,173 +1,112 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import Navbar from '../../components/common/navbar/Navbar';
+import {
+  CometChatConversationListWithMessages,
+  CometChatConversationList,
+  CometChatUI,
+} from '../../view/cometchat-pro-react-ui-kit-master/CometChatWorkspace/src';
+import { CometChat } from '@cometchat-pro/react-native-chat';
+import { COMETCHAT_CONSTANTS } from '../../config/COMMET_CONSTANT';
+// import fire from '../../config/config';
 
-const ChatScreen = () => {
+
+export default function HostMessages({ navigation, route }) {
+  const { recieverId } = route.params;
+  const CurrentUserId = '4564564564646546';
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [chatWithUser, setChatWithUser] = useState('');
+  const [currentUserId, setCurrentUSERID] = useState('');
+  const [tId, setTID] = useState('');
+  // console.log('user', userId)
+  useEffect(() => {
+    if (CurrentUserId) {
+      var appSetting = new CometChat.AppSettingsBuilder()
+        .subscribePresenceForAllUsers()
+        .setRegion(COMETCHAT_CONSTANTS.REGION)
+        .build();
+
+      CometChat.init(COMETCHAT_CONSTANTS.APP_ID, appSetting).then(
+        () => {
+          if (CometChat.setSource) {
+            CometChat.setSource('ui-kit', 'web', 'reactjs');
+
+            // console.log("OWN USER", userId)
+
+            CometChat.login(CurrentUserId, COMETCHAT_CONSTANTS?.AUTH_KEY)
+              .then(user => {
+                setIsInitialized(true);
+                let limit = 30;
+                let conversationRequest =
+                  new CometChat.ConversationsRequestBuilder()
+                    .setLimit(limit)
+                    .build();
+                conversationRequest.fetchNext().then(conversationList => {
+                  if (conversationList?.length > 0) {
+                    if (!currentUserId) {
+                      setChatWithUser(
+                        conversationList[0]?.getConversationWith?.uid,
+                      );
+                    }
+                  }
+                });
+              })
+              .catch(error => {
+                if (error.code === 'ERR_UID_NOT_FOUND') {
+                  console.log('Error', error.code);
+                  const uid = CurrentUserId;
+                  // console.log("UID", uid)
+                  let name = 'Hassan HOST';
+                  // let image = data.data.user?.image;
+                  var user = new CometChat.User(uid);
+                  user.setName(name);
+                  // if (image) {
+                  //     user.setAvatar(image);
+                  // }
+                  CometChat.createUser(
+                    user,
+                    COMETCHAT_CONSTANTS?.AUTH_KEY,
+                  ).then(
+                    user => {
+                      CometChat.login(
+                        CurrentUserId,
+                        COMETCHAT_CONSTANTS?.AUTH_KEY,
+                      ).then(user_ => {
+                        console.log('LOGIN', user_);
+                      });
+                    },
+                    error => {
+                      console.log('ERROR', error);
+                    },
+                  );
+                }
+              });
+          }
+        },
+        error => { },
+      );
+    }
+  });
+
+  useEffect(() => {
+    if (CurrentUserId) {
+      setCurrentUSERID(CurrentUserId);
+    }
+    // if (location.state?.travelerID) {
+    //   setTID(location.state?.travelerID);
+    // }
+  }, [CurrentUserId]);
+
+  console.log('Opposite USER', tId, 'OWN ID', CurrentUserId);
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
+    isInitialized && (
+      <View>
+        <CometChatUI
+          conversationWith={recieverId}
+        // chatWithUser={chatWithUser}
+        />
+      </View>
+    )
+  );
 }
-
-export default ChatScreen
-// import React, { useState, useCallback, useEffect } from 'react'
-// import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat'
-// import {View, Text, Image, LogBox} from 'react-native'
-// import useAuth from '../../Hooks/useAuth';
-// // import {auth} from '@react-native-firebase/auth'
-// import firestore from '@react-native-firebase/firestore';
-// import {Color} from '../../Utils/colorfile'
-// import {useNavigation} from '@react-navigation/core';
-// import tempImg from '../../assets/image6.png'
-
-// // import FirebaseData from '../../Utils/FirebaseData'; 
-// const ChatScreen = ({route})=>{
-//   const [messages, setMessages] = useState([]);
-//   const {user} = useAuth()
-//   const {recieverId} = route.params;
-//   const navigation = useNavigation();
-//   console.log('reciever',JSON.stringify(recieverId))
-//   // console.log('con', user)
-//   const getAllMessages = async()=>{
-//     const docid =
-//     recieverId > user 
-//     ? user + "-" + recieverId
-//     : recieverId + "-" + user;
-//     const querySanp = await firestore()
-//     .collection('chats')
-//     .doc(docid)
-//     .collection('messages')
-//     .orderBy('createdAt', 'desc')
-//     .get();
-//   const allmsg = querySanp.docs.map(docSanp => {
-//     return {
-//       ...docSanp.data(),
-//       createdAt: docSanp.data().createdAt.toDate(),
-//     };
-//   });
-//   setMessages(allmsg);
-// // };
-//   }
-//   useEffect(() => {
-//   LogBox.ignoreLogs(['EventEmitter.removeListener'])
-//   getAllMessages()
-//   navigation.setOptions({
-//     title: (
-//       <View style={{backgroundColor:Color.PRIMARY_COLOR, flexDirection:'row', }}>
-//         <View style={{ marginLeft:-20}} >
-//         <Image source ={tempImg} style={{width:40, height:40, borderRadius:50 }}  />
-//         </View>
-//         <View style={{marginLeft:5 ,backgroundColor:Color.PRIMARY_COLOR,}}>
-//         <Text style={{color: '#fff', fontFamily: 'Arciform', fontSize: 16.5}}>
-//           {recieverId}
-//         </Text>
-//         <Text
-//           style={{
-//             color: Color.BACKGROUND_COLOR,
-//             fontFamily: 'Arciform',
-//             fontSize: 15,
-//           }}>
-//           online
-//         </Text>
-
-//         </View>
-//       </View>
-//     ),})
-//     const docid =
-//       recieverId > user
-//         ? user + '-' + recieverId
-//         : recieverId + '-' + user
-//     const messageRef = firestore()
-//       .collection('chats')
-//       .doc(docid)
-//       .collection('messages')
-//       .orderBy('createdAt', 'desc');
-
-//     const unSubscribe = messageRef.onSnapshot(querySnap => {
-//       const allmsg = querySnap.docs.map(docSanp => {
-//         const data = docSanp.data();
-//         if (data.createdAt) {
-//           return {
-//             ...docSanp.data(),
-//             createdAt: docSanp.data().createdAt.toDate(),
-//           };
-//         } else {
-//           return {
-//             ...docSanp.data(),
-//             createdAt: new Date(),
-//           };
-//         }
-//       });
-//       setMessages(allmsg);
-//     });
-//     return () => {
-//       unSubscribe();
-//     };
-//   }, [])
- 
-//   // const onSend = useCallback((messages = []) => {
-//   //   setMessages(previousMessages => GiftedChat.append(previousMessages, messages));  
-//   // }, [])
-//   const onSend = messageArray => {
-//     const msg = messageArray[0];
-//     const mymsg = {
-//       ...msg,
-//       sentBy: user,
-//       sentTo: recieverId,
-//       createdAt: new Date(),
-//     };
-//     setMessages(previousMessages => GiftedChat.append(previousMessages, mymsg));
-//     const docid =
-//       recieverId > user
-//         ? user + '-' + recieverId
-//         : recieverId + '-' + user;
-
-//     firestore()
-//       .collection('Users')
-//       .doc(docid)
-//       .collection('messages')
-//       .add({...mymsg, createdAt: firestore.FieldValue.serverTimestamp()});
-//   };
-
-//   return (
-//     <View style={{flex: 1, backgroundColor: Color.BACKGROUND_COLOR}}>
-    
-//     <GiftedChat
-//       messages={messages}
-//       onSend={messages => onSend(messages)}
-//       user={{
-//         _id: user,
-//       }}
-//       renderBubble={props => {
-//         return (
-//           <Bubble
-//             {...props}
-//             wrapperStyle={{
-//               right: {
-//                 backgroundColor: Color.PRIMARY_COLOR,
-//               },
-//               left: {
-//                 backgroundColor: Color.SECONDARY_COLOR,
-//               },
-//             }}
-//           />
-//         );
-//       }}
-//       renderInputToolbar={props => {
-//         return (
-//           <InputToolbar
-//             {...props}
-//             containerStyle={{
-//               borderTopWidth: 1,
-//               borderTopColor: Color.PRIMARY_COLOR,
-//             }}
-//             textIn-putStyle={{color: Color.BLACK}}
-//           />
-//         );
-//       }}
-//       />
-
-//       </View>
-//   )
-// }
-// export default ChatScreen
